9e394bada45df7dbddbb3d73412575a4eb9772cd
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2017-09-25 15:24:03 +0100

    submitting instead of clicking for dev

--- functional_tests/tests_example.py
+++ functional_tests/tests_example.py
@@ -9,8 +9,6 @@ class HmmerExampleTestCase(HmmerWebTest):
         browser.find_element_by_css_selector("#example").click()
         self.assertNotEqual("", textarea.text, "The textarea should NOT be empty after clicking the example link")
 
-        browser.find_element_by_css_selector(".close-button").click()
-
-        click_link_and_wait(browser.find_element_by_id("subbutton"))
+        browser.find_element_by_id("subbutton").submit()
         title = browser.find_element_by_css_selector("div.row div.columns>h5")
         self.assertEqual("PHMMER Results", title.text, "The result title should be PHMMER Results")
