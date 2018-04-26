01ced08fbe2f18a1657e2f1cfff85d0d6c598f35
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-04-26 17:04:09 +0100

    fixing issue with cookies banner

--- functional_tests/tests_example.py
+++ functional_tests/tests_example.py
@@ -1,5 +1,5 @@
-from test import HmmerWebTest, click_link_and_wait
-
+from test import HmmerWebTest, click_link_and_wait, ACTION_SUBMIT
+import time
 
 class HmmerExampleTestCase(HmmerWebTest):
     def test_hmmer_search_example(self):
@@ -8,9 +8,16 @@ class HmmerExampleTestCase(HmmerWebTest):
         textarea = browser.find_element_by_id("seq")
         self.assertEqual("", textarea.text, "The textarea should be empty when landing in the homepage")
         browser.find_element_by_css_selector("#example").click()
+        time.sleep(5)
         self.assertNotEqual("", textarea.text, "The textarea should NOT be empty after clicking the example link")
 
-        b = browser.find_element_by_id("subbutton") #.submit()
+        try:
+          browser.find_element_by_css_selector("#cookie-banner button").click()
+        except: pass
+
+        b = browser.find_element_by_id("subbutton")
+        browser.execute_script("return arguments[0].scrollIntoView();", b)
+        browser.execute_script("window.scrollBy(0, -150);")
         click_link_and_wait(b)
         title = browser.find_element_by_css_selector("div.row div.columns>h5")
         self.assertEqual("PHMMER Results", title.text, "The result title should be PHMMER Results")
