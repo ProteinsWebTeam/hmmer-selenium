9500905f1fe6b7d322ed775b618e6e3547f5d1fc
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2020-07-06 15:19:11 +0100

    support for textarea-sequence

--- functional_tests/tests_example.py
+++ functional_tests/tests_example.py
@@ -1,11 +1,17 @@
 from test import HmmerWebTest, click_link_and_wait, ACTION_SUBMIT
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 class HmmerExampleTestCase(HmmerWebTest):
     def test_hmmer_search_example(self):
         browser = self.driver
         browser.get(self.hmmer_home_page)
-        textarea = browser.find_element_by_id("seq")
+        try:
+            textarea = browser.find_element_by_id("textarea_seq")
+            usingWebComponent = True
+        except NoSuchElementException:
+            textarea = browser.find_element_by_id("seq")
+            usingWebComponent = False
         self.assertEqual("", textarea.text, "The textarea should be empty when landing in the homepage")
         browser.find_element_by_css_selector("#example").click()
         time.sleep(5)
